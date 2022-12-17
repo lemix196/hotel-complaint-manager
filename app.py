ceb0aca298bab9cf545f2ee5d66272abea7a1d00
glemiecha <g.lemiecha1@gmail.com>
2022-12-17 12:05:41 +0100

    add planned routes to app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
@@ -6,6 +6,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "This is index page"
+
+@app.route('/complaint', methods=['GET', 'POST'])
+def complaint():
+    return "This is complaint page"
+
+@app.route('/complaints/<user>')
+def index():
+    return "This is user complaints page"
+
+@app.route('/ratings')
+def index():
+    return "This is ratings page"
+
 
 
 if __name__ == "__main__":
