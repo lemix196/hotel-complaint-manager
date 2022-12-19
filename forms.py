bd6a00e5eceb5d30682962c55a35d8bcf36e3ab7
glemiecha <g.lemiecha1@gmail.com>
2022-12-19 10:31:42 +0100

    add forms.py with complaint form class

---
+++ forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SelectField
+
+#later on add validators to that class
+class ComplaintForm(FlaskForm):
+
+    _URGENCY_LIST = [("normal", "normal"),
+                     ("high", "high"),
+                     ("emergency", "emergency")
+                    ]
+
+    guest_name = StringField(u"Full guest name")
+    room_number = IntegerField(u"Room number")
+    message = StringField(u"Tell us about your inconvenience")
+    urgency = SelectField(u"Urgency...", choices=_URGENCY_LIST)
+
+
+if __name__ == "__main__":
+    pass
