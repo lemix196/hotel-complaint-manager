a147a3e1125dd00449f4603705c769e18133d9ae
glemiecha <g.lemiecha1@gmail.com>
2022-12-23 15:55:21 +0100

    create app database hcm.db, add logic to addition records to DB from complaint form

--- app.py
+++ app.py
@@ -1,21 +1,46 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, request
 from forms import ComplaintForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "f2f815314d859772ef6afe68"
+app.config.from_pyfile('config.cfg')
+db = SQLAlchemy(app)
+
+# DB Models
+
+class Complaint(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    guest_name = db.Column(db.String(50))
+    room_number = db.Column(db.Integer)
+    message = db.Column(db.String(500))
+    urgency = db.Column(db.String(15))
+
+
+# End of DB models
 
 @app.route('/')
 def index():
+    db.create_all()
     return "This is index page"
 
-@app.route('/complaint', methods=['GET', 'POST'])
+@app.route('/complaint', methods=['POST', 'GET'])
 def complaint():
     form = ComplaintForm()
+    if request.method == "POST" and form.validate_on_submit():
+        complaint = Complaint(guest_name=form.guest_name.data,
+                              room_number=form.room_number.data,
+                              message=form.message.data,
+                              urgency=form.urgency.data)
+        db.session.add(complaint)
+        db.session.commit()
+        flash('Your complaint was succesfully sent.')
+        return render_template('complaint_summary.html', form=form)
+    
     return render_template('complaint.html', form=form)
 
-@app.route('/complaints/<user>')
-def list_user_complaints():
-    return "This is user complaints page"
+# @app.route('/complaints/<user>')
+# def list_user_complaints():
+#     return "This is user complaints page"
 
 @app.route('/ratings')
 def ratings():
