cd7504c17384ba356388bb890788c8b6ea29474e
glemiecha <g.lemiecha1@gmail.com>
2023-01-23 19:11:00 +0100

    move users.py into models directory

---
+++ models/users.py
@@ -0,0 +1,31 @@
+from .models import db
+import os
+import hashlib
+
+
+class User:
+    def __init__(self, user_name, password, group='guest'):
+        self.user_name = user_name
+        self.password = password
+        self.group = group
+
+    def hash_password(self, password):
+        salt = os.urandom(32)
+        key = hashlib.pbkdf2_hmac(
+            'sha256',
+            password.encode('utf-8'),
+            salt,
+            110996
+        )
+        return salt + key
+
+    def verify_password(self, stored_password, provided_password):
+        salt = stored_password[:32]
+        provided_password = salt + hashlib.pbkdf2_hmac(
+            'sha256',
+            provided_password.encode('utf-8'),
+            salt,
+            110996
+        )
+        return provided_password == stored_password
+
